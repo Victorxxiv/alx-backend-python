506d53e88bd93f1716ea66d6ad22202f5c3604c9
Victor <victormwendwa804@gmail.com>
2024-06-17 18:10:49 +0000

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module that contains a single function that concatenates two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings
+
+    Args:
+        str1 (str): The first string.
+        str2 (str): The second string.
+
+    Returns:
+        str: The concatenated string.
+    """
+    return str1 + str2
